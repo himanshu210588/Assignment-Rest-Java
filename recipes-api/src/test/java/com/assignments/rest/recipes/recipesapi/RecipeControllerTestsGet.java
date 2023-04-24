7b62b50deeb306b1ca2787b53c4d5d85cfe0d252
himanshu210588 <himanshutiwari21@hotmail.com>
2023-04-24 03:15:34 +0200

    Implemented few test cases to test controller methods by mocking RecipeDaoService

--- recipes-api/src/test/java/com/assignments/rest/recipes/recipesapi/RecipeControllerTestsGet.java
+++ recipes-api/src/test/java/com/assignments/rest/recipes/recipesapi/RecipeControllerTestsGet.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
--- recipes-api/src/test/java/com/assignments/rest/recipes/recipesapi/RestControllerTestsPostCsrf.java
+++ recipes-api/src/test/java/com/assignments/rest/recipes/recipesapi/RestControllerTestsPostCsrf.java
@@ -19,7 +19,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.client.RestTemplate;
 
 import com.assignments.rest.recipes.recipesapi.beans.Recipe;
 import com.assignments.rest.recipes.recipesapi.controllers.RecipeController;
